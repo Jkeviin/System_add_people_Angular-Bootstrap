2dd52dfce972baa0f69a315d333b178ab89abd67
Kevin <slash2130kevin@gmail.com>
2022-10-24 12:37:47 -0500

    Listado persona terminado, solo agrega

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Persona } from './persona.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'listado-personas';
+  titulo = 'Listado de Personas';
+  personas: Persona[] = [
+      new Persona('Kevin', 'Ortega'),
+      new Persona('Juan', 'Perez'),
+      new Persona('Daniel', 'Alvares')
+    ];
+  nombreInput: string = '';
+  apellidoInput: string = '';
+
+  public agregarPersona(){
+    if(this.nombreInput != '' && this.apellidoInput != ''){
+      let persona1 = new Persona(this.nombreInput, this.apellidoInput);
+      this.personas.push(persona1);
+    }else{
+      alert('Debe ingresar los datos');
+    }
+
+  }
 }
---
+++ src/app/persona.model.ts
@@ -0,0 +1,17 @@
+export class Persona{
+/*   ombre: string = "";
+    apellido: string = "";
+
+    constructor(nombre: string, apellido: string){
+        this.nombre = nombre;
+        this.apellido = apellido;
+    } */
+
+    // Lo anterior se puede simplificar con lo siguiente:
+
+    constructor(public nombre: string, public apellido: string){
+        
+    }
+
+
+}
