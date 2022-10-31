d49985837b41b27fc76c5f587a8b924522e3d528
Kevin <slash2130kevin@gmail.com>
2022-10-30 22:00:49 -0500

    Routing Module y Routing-oulet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { PersonasComponent } from './componentes/personas/personas.component';
+import { FormularioComponent } from './componentes/personas/formulario/formulario.component';
 
-
+const routes:  Routes = [
+  { path: '', component: PersonasComponent, pathMatch: 'full'},   // pathMatch: 'full' es para que solo se muestre el componente cuando la ruta sea exactamente igual a la ruta del compnente
+  { path: 'personas', component: PersonasComponent, pathMatch: 'full'},
+  { path: 'personas/agregar', component: FormularioComponent, pathMatch: 'full'},
+  { path: 'personas/:id', component: FormularioComponent, pathMatch: 'full'}
+]
 
 @NgModule({
-  declarations: [],
   imports: [
-    CommonModule
-  ]
+    RouterModule.forRoot(
+      routes
+      )
+  ],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
